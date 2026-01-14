7c890e152272ba3036d14cb928c6e33c1868c685
María Pérez Portero <mperezg@fpvirtualaragon.es>
2026-01-14 23:17:58 +0100

    Mod. urls app principal

--- gestion_tareas/gestion_tareas/urls.py
+++ gestion_tareas/gestion_tareas/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+
+
 
 urlpatterns = [
-    # Verifica que diga views.tareas_alumno
-    path('alumno/', views.tareas_alumno, name='tareas_alumno'),
-    path('profe/', views.tareas_pendientes_validacion, name='tareas_profesor'),
-    path('completar/<int:tarea_id>/', views.completar_tarea, name='completar_tarea'),
-    path('validar/<int:tarea_id>/', views.validar_tarea, name='validar_tarea'),
+    
+    path('admin/', admin.site.urls),
+    path('', include('tareas.urls')),
+    path('usuarios/', include('usuarios.urls')),
+    
 ]
